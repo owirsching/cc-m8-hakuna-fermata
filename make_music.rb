77f3c7d0f09346423f9c5d3e64eefb87fb68d3de
owirsching <olivia.wirsching@gmail.com>
2023-12-06 20:30:32 -0500

    Final

--- make_music.rb
+++ make_music.rb
@@ -1,6 +1,9 @@
-# Welcome to Sonic Pi
+# Olivia Wirsching
+# Computational Creativity M8: Hakuna Fermata
+# December 7, 2023
 
 use_synth :piano
+use_bpm 65
 
 # Uplifting progression to start the study session
 play 63, sustain: 0.25, attack: 0.5, release: 0.5
@@ -14,173 +17,124 @@ sleep 0.1
 play 75, sustain: 0.25, attack: 0.5, release: 0.5
 sleep 1.00
 
+# Relaxing BPM
+use_bpm 35
 
-use_synth :dark_ambience
-in_thread do
-  live_loop :cosmic_light do
+# Flowy background
+define :flow do
+  use_synth :dark_ambience
+  2.times do
     use_bpm 65
     
-    play chord(:C4, :maj), attack: 2, release: 9, sustain: 2.5
+    play chord(:C4, :maj), attack: 2, release: 9, sustain: 2.5, amp: 0.5
     sleep(5)
-    play invert_chord(chord(:G4, :maj), 1), attack: 2, release: 9, sustain: 2.5
+    play invert_chord(chord(:G4, :maj), 1), attack: 2, release: 9, sustain: 2.5, amp: 0.5
     sleep(5)
-    play chord(:a4, :minor), attack: 2, release: 9, sustain: 5
+    play chord(:a4, :minor), attack: 2, release: 9, sustain: 5, amp: 0.5
     sleep(5)
-    play invert_chord(chord(:A4, :min), 1), attack: 2, release: 9, sustain: 2.5
+    play invert_chord(chord(:A4, :min), 1), attack: 2, release: 9, sustain: 2.5, amp: 0.5
     sleep(5)
-    play invert_chord(chord(:A4, :min), 1), attack: 2, release: 9, sustain: 2.5
+    play invert_chord(chord(:A4, :min), 1), attack: 2, release: 9, sustain: 2.5, amp: 0.5
     sleep(5)
-    play invert_chord(chord(:C6, :maj), 1), attack: 2, release: 9, sustain: 2.5
+    play invert_chord(chord(:C6, :maj), 1), attack: 2, release: 9, sustain: 2.5, amp: 0.5
     sleep(5)
     tempo -= 1
   end
 end
 
-use_bpm 35
-
-in_thread do
+# Soft notes
+define :background_melody do
   sleep 2
   4.times do
     use_synth :dark_ambience
     play chord(:C4, :maj), amp: 2
-    sleep 0.3
+    sleep 0.4
     play chord(:G4, :maj), amp: 2
-    sleep 0.3
+    sleep 0.4
     play chord(:A4, :min), amp: 2
-    sleep 0.3
+    sleep 0.4
     play chord(:F4, :maj), amp: 2
-    sleep 0.3
+    sleep 0.4
     play chord(:C6, :maj)
-    sleep 0.3
+    sleep 0.4
     play chord(:G6, :maj)
-    sleep 0.3
+    sleep 0.4
     play chord(:A6, :min)
-    sleep 0.3
+    sleep 0.4
     play chord(:F6, :maj)
     sleep 1.0
   end
 end
 
-##| in_thread do
-##|   sleep 2
-##|   live_loop :saw do
-##|     use_synth :saw
-##|     play :G5, amp: 0.05, attack: 0.5
-##|     sleep 0.3
-##|     play :D5, amp: 0.05, attack: 0.5
-##|     sleep 0.3
-##|     play :C5, amp: 0.05, attack: 0.5
-##|     sleep 0.3
-##|     play :C5, amp: 0.05, attack: 0.5
-##|     sleep 0.3
-
-##|     play chord(:A5, :min), amp: 0.05
-##|     sleep 0.3
-##|     play chord(:G5, :maj), amp: 0.05
-##|     sleep 0.2
-##|     play chord(:E5, :min), amp: 0.05
-##|     sleep 0.2
-##|     play chord(:F5, :maj), amp: 0.05
-##|     sleep 0.3
-##|   end
-##| end
-
-in_thread do
+# Flowing piano notes
+define :piano_notes do
   sleep 10
   2.times do
     use_synth :piano
     play :F4, amp: 2, attack: 0.5, release: 0.5
-    sleep 0.3
+    sleep 0.4
     play :G4, amp: 2, attack: 0.5, release: 0.5
-    sleep 0.3
+    sleep 0.4
     play :B4, amp: 3, attack: 0.5, release: 0.5
-    sleep 0.3
+    sleep 0.4
     play :A4, amp: 3, attack: 0.5, release: 0.5
-    sleep 0.3
+    sleep 0.4
     play :C5, amp: 4, attack: 0.5, release: 0.5
-    sleep 0.3
+    sleep 0.4
     play :A4, amp: 3, attack: 0.5, release: 0.5
-    sleep 0.3
+    sleep 0.4
     play :B4, amp: 3, attack: 0.5, release: 0.5
-    sleep 0.3
+    sleep 0.4
     play :G4, amp: 2, attack: 0.5, release: 0.5
-    sleep 0.3
+    sleep 0.4
   end
   
   2.times do
     use_synth :piano
     play :F4, amp: 2, attack: 0.5, release: 0.5
-    sleep 0.3
+    sleep 0.4
     play :G4, amp: 2, attack: 0.5, release: 0.5
-    sleep 0.3
+    sleep 0.4
     play :B4, amp: 3, attack: 0.5, release: 0.5
-    sleep 0.3
+    sleep 0.4
     play :A4, amp: 3, attack: 0.5, release: 0.5
-    sleep 0.3
+    sleep 0.4
     play chord(:C5, :major), amp: 7, attack: 1, release: 1, sustain: 1
     play chord(:C4, :major), amp: 7, attack: 1, release: 1, sustain: 1
-    sleep 0.3
+    sleep 0.4
     play :A4, amp: 3, attack: 0.5, release: 0.5
-    sleep 0.3
+    sleep 0.4
     play :B4, amp: 3, attack: 0.5, release: 0.5
-    sleep 0.3
+    sleep 0.4
     play :G4, amp: 2, attack: 0.5, release: 0.5
-    sleep 0.3
+    sleep 0.4
   end
   
-  6.times do
+  4.times do
     use_synth :piano
     play :F3, amp: 2, attack: 0.5, release: 0.5
-    sleep 0.3
+    sleep 0.4
     play :G3, amp: 2, attack: 0.5, release: 0.5
-    sleep 0.3
+    sleep 0.4
     play :B3, amp: 3, attack: 0.5, release: 0.5
-    sleep 0.3
+    sleep 0.4
     play :A3, amp: 3, attack: 0.5, release: 0.5
-    sleep 0.3
+    sleep 0.4
     play chord(:C4, :major), amp: 7, attack: 1, release: 1, sustain: 1
     play chord(:C3, :major), amp: 7, attack: 1, release: 1, sustain: 1
-    sleep 0.3
+    sleep 0.4
     play :A3, amp: 3, attack: 0.5, release: 0.5
-    sleep 0.3
+    sleep 0.4
     play :B3, amp: 3, attack: 0.5, release: 0.5
-    sleep 0.3
+    sleep 0.4
     play :G3, amp: 2, attack: 0.5, release: 0.5
-    sleep 0.3
-  end
-end
-
-in_thread do
-  live_loop :p do
-    sample :bass_dnb_f
     sleep 0.4
   end
 end
 
-
-in_thread do
-  live_loop :p do
-    use_bpm 65
-    use_synth :piano
-    play chord(:C4, :maj), attack: 2, release: 9, sustain: 2.5
-    sleep(5)
-    play invert_chord(chord(:G4, :maj), 1), attack: 2, release: 9, sustain: 2.5
-    sleep(5)
-    play chord(:a4, :maj), attack: 2, release: 9, sustain: 5
-    sleep(5)
-    play invert_chord(chord(:A3, :maj), 1), attack: 2, release: 9, sustain: 2.5
-    sleep(5)
-    play invert_chord(chord(:A3, :maj), 1), attack: 2, release: 9, sustain: 2.5
-    sleep(5)
-    play invert_chord(chord(:C3, :maj), 1), attack: 2, release: 9, sustain: 2.5
-    sleep(5)
-    tempo -= 1
-  end
-end
-
-in_thread do
-  sleep 20
-  live_loop :chords do
+# Low background noise
+define :chords do
+  loop do
     use_synth :dark_ambience
     play chord(:C3, :maj), attack: 1, release: 2, sustain: 1
     sleep 2
@@ -191,23 +145,138 @@ in_thread do
   end
 end
 
-in_thread do
-  sleep 30
+# Low background noise
+define :dark_transition do
   use_synth :dark_ambience
-  play chord(:C3, :maj), attack: 3, release: 3, sustain: 4, amp: 3
+  play chord(:C3, :maj), attack: 3, release: 3, sustain: 4, amp: 0.5
   sleep 8
-  play chord(:C5, :maj), attack: 3, release: 3, sustain: 4, amp: 3
-  use_synth :piano
+  play chord(:C5, :maj), attack: 3, release: 3, sustain: 4, amp: 0.5
 end
 
-in_thread do
-  sleep 35
-  live_loop :drums do
-    sample :drum_bass_hard, rate: 1.2, amp: 0.25
+# Piano chords
+define :second_piano do
+  5.times do
+    use_synth :piano
+    play :G4, amp: 5, attack: 0.75, release: 0.75
+    sleep 0.5
+    play chord(:G4, :major), amp: 10, attack: 1, release: 1
+    sleep 0.75
+    play :B4, amp: 5, attack: 0.75, release: 0.75
+    sleep 0.5
+    play chord(:B4, :minor), amp: 10, attack: 1, release: 1
+    sleep 0.75
+    play :D4, amp: 5, attack: 0.75, release: 0.75
+    sleep 0.5
+    play chord(:D4, :major), amp: 10, attack: 1, release: 1
+    sleep 2
+  end
+  
+  5.times do
+    use_synth :piano
+    play :G4, amp: 5, attack: 0.75, release: 0.75
+    sleep 0.75
+    play chord(:G4, :major), amp: 10, attack: 1, release: 1
     sleep 0.4
-    sample :drum_bass_hard, rate: 1.2, amp: 0.25
-    sleep 0.2
-    sample :drum_bass_hard, rate: 1.5, amp: 0.25
+    harp_up
+    sleep 0.4
+    play :B4, amp: 5, attack: 0.75, release: 0.75
+    sleep 0.75
+    play chord(:B4, :minor), amp: 10, attack: 1, release: 1
+    sleep 0.4
+    harp_up
+    sleep 0.4
+    play :D4, amp: 5, attack: 0.75, release: 0.75
+    sleep 0.75
+    play chord(:D4, :major), amp: 10, attack: 1, release: 1
+    sleep 0.4
+    play :D4, attack: 0.5, release: 0.5, amp: 3
     sleep 0.4
   end
 end
+
+# Quickly rising notes
+define :harp_up do
+  ##| sleep 50
+  use_synth :piano
+  play :B5, attack: 0.5, release: 0.5, amp: 2
+  sleep 0.01
+  play :G5, attack: 0.5, release: 0.5, amp: 2
+  sleep 0.01
+  play :D5, attack: 0.5, release: 0.5, amp: 2
+end
+
+
+# FIRST PART
+in_thread do
+  in_thread do
+    cosmic_light
+  end
+  
+  in_thread do
+    background_melody
+  end
+  
+  in_thread do
+    piano_notes
+  end
+end
+
+# SECOND PART
+in_thread do
+  sleep 20
+  in_thread do
+    chords
+  end
+  
+  in_thread do
+    sleep 10
+    dark_transition
+  end
+  
+  in_thread do
+    sleep 20
+    second_piano
+  end
+end
+
+# THIRD PART
+in_thread do
+  sleep 95
+  in_thread do
+    cosmic_light
+  end
+  
+  in_thread do
+    3.times do
+      play :B5, attack: 0.5, release: 0.5, amp: 4
+      sleep 0.5
+      play :G5, attack: 0.5, release: 0.5, amp: 4
+      sleep 0.5
+      play :D5, attack: 0.5, release: 0.5, amp: 4
+      sleep 3
+      play :D5, attack: 0.5, release: 0.5, amp: 4
+      sleep 0.5
+      play :G5, attack: 0.5, release: 0.5, amp: 4
+      sleep 0.5
+      play :B5, attack: 0.5, release: 0.5, amp: 4
+      sleep 5
+    end
+  end
+end
+
+# FOURTH PART
+in_thread do
+  sleep 115
+  in_thread do
+    piano_notes
+  end
+  
+  sleep 20
+  in_thread do
+    background_melody
+  end
+end
+
+
+
+
